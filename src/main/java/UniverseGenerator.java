d130b3240fe1a61d760f41518d5aac2412b2b596
longnguyen268 <shmalltoise@gmail.com>
2026-02-18 20:32:58 -0500

    Merged PathFinder and worked on UniverseGenerator

--- src/main/java/UniverseGenerator.java
+++ src/main/java/UniverseGenerator.java
@@ -18,8 +18,36 @@ public class UniverseGenerator {
         universe.addEdge(p1, p2, 700);
 
         // --- MILESTONE 2: PROCEDURAL GENERATION ---
+        ArrayList<Planet> planets = new ArrayList<>();
+
         // TODO: Loop 20 times to create random planets.
-        // TODO: Check for collisions (min distance > 80).
+        for(int i = 0; i < 20; i++){
+            Planet newPlanet = null;
+            int attempt = 0;
+            boolean validPosition = true;
+
+            while(validPosition && attempt < 100){
+                String name = "Planet" + (i+1);
+                double x = (Math.random()*(width - 100));
+                double y = (Math.random()*(height - 100));
+
+                newPlanet = new Planet(name, x, y);
+                // TODO: Check for collisions (min distance > 80).
+                for(Planet planet: planets){
+                    double distance = Math.sqrt(Math.pow(planet.getX(), 2) + (Math.pow(planet.getY(), 2)));
+                    if(distance < 80){
+                        validPosition = false;
+                        break;
+                    }
+                }
+                attempt++;
+            }
+            if(validPosition){
+                planets.add(newPlanet);
+                universe.addVertex(newPlanet);
+            }
+        }
+
         // TODO: Connect each planet to its 3 closest neighbors.
 
         return universe;
